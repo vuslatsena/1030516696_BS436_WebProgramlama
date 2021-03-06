2de385a803181f3102193d7595a4d0123c6d3392
VuslatSena <vuslatsena23@gmail.com>
2021-03-06 12:11:37 +0300

    Odev1_Son

--- Odev1/Script.js
+++ Odev1/Script.js
@@ -4,18 +4,18 @@ let page2;
 let guess = 2;
 let latestIndex;
 
-let sImages =  ["images/cat.png","images/dog.jpg","images/dog1.jpg"];
+let cards =  ["images/cat.png","images/dog.jpg","images/dog1.jpg"];
 
 const newGame = () => {
     if(guess!==0){
-        let index = Math.floor(Math.random()*sImages.length);
+        let index = Math.floor(Math.random()*cards.length);
         if(latestIndex === index){
-            index = (index+1)%sImages.length;
+            index = (index+1)%cards.length;
         }
-        const image = sImages[index];
+        const image = cards[index];
         latestIndex = index;
         page = document.getElementById("img").src=image;
-        if(sImages[0] === image){
+        if(cards[0] === image){
             document.getElementById("alanId").innerHTML="Kazandınız! Tebrik ederiz :) Yeni bir oyun oynamak isterseniz " +
                 "<span class='link' onclick='document.location.reload(true);'> buraya </span> tıklayabilirsiniz.";
         }
@@ -29,15 +29,15 @@ const newGame = () => {
 
 const newGame1 = () => {
     if(guess!==0){
-        let index1 = Math.floor(Math.random()*sImages.length);
+        let index1 = Math.floor(Math.random()*cards.length);
         if(latestIndex === index1){
-            index1 = (index1+1)%sImages.length;
+            index1 = (index1+1)%cards.length;
         }
-        const image1 = sImages[index1];
+        const image1 = cards[index1];
         latestIndex = index1;
 
         page1 = document.getElementById("img1").src=image1;
-        if(sImages[0] === image1){
+        if(cards[0] === image1){
             document.getElementById("alanId").innerHTML="Kazandınız! Tebrik ederiz :) Yeni bir oyun oynamak isterseniz " +
                 "<span class='link' onclick='document.location.reload(true);'> buraya </span> tıklayabilirsiniz.";
         }
@@ -50,14 +50,14 @@ const newGame1 = () => {
 }
 const newGame2 = () => {
     if(guess!==0){
-        let index2 = Math.floor(Math.random()*sImages.length);
+        let index2 = Math.floor(Math.random()*cards.length);
         if(latestIndex === index2){
-            index2 = (index2+1)%sImages.length;
+            index2 = (index2+1)%cards.length;
         }
-        const image2 = sImages[index2];
+        const image2 = cards[index2];
         latestIndex = index2;
         page2 = document.getElementById("img2").src=image2;
-        if(sImages[0] === image2){
+        if(cards[0] === image2){
             document.getElementById("alanId").innerHTML="Kazandınız! Tebrik ederiz :) Yeni bir oyun oynamak isterseniz " +
                 "<span class='link' onclick='document.location.reload(true);'> buraya </span> tıklayabilirsiniz.";
         }
