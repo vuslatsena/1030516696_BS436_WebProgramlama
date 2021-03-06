e1176ff8c16f14ba0cb51610b43175134deb52f0
VuslatSena <vuslatsena23@gmail.com>
2021-03-06 11:17:00 +0300

    Odev01_Son

--- Odev1/Script.js
+++ Odev1/Script.js
@@ -1,64 +1,64 @@
-const secondImages = [
-    "cat.png",
-    "dog.jpg",
-];
-
 let guess = 2;
 let latestIndex;
-let html;
-let html1;
-let html2;
+let page;
+let page1;
+let page2;
+
+const sImages = ["cat.png","dog.jpg",];
 
 const newGame = () => {
     if(guess!==0){
-        let index = Math.floor(Math.random()*secondImages.length);
+        let index = Math.floor(Math.random()*sImages.length);
         if(latestIndex === index){
-            index = (index+1)%secondImages.length;
+            index = (index+1)%sImages.length;
         }
-        const image = secondImages[index];
+        const image = sImages[index];
         latestIndex = index;
-        html = document.getElementById("img0").src=image;
-        if(secondImages[0] === image){
+        page = document.getElementById("img").src=image;
+        if(sImages[0] === image){
             document.getElementById("alanId").innerHTML="Kazandın!!! Tebrik ederiz :) Yeni bir oyun oynamak istersen <span class='link' onclick='document.location.reload(true);'>buraya</span> tıklayabilirsin.";
         }
         guess-=1;
-    }else{
+    }
+    else{
         document.getElementById("alanId").innerHTML="Kaybettin :( Yeni bir oyun oynamak istersen <span class='link' onclick='document.location.reload(true);'>buraya</span> tıklayabilirsin.";
     }
 }
 
 const newGame1 = () => {
     if(guess!==0){
-        let index1 = Math.floor(Math.random()*secondImages.length);
+        let index1 = Math.floor(Math.random()*sImages.length);
         if(latestIndex === index1){
-            index1 = (index1+1)%secondImages.length;
+            index1 = (index1+1)%sImages.length;
         }
-        const image1 = secondImages[index1];
+        const image1 = sImages[index1];
         latestIndex = index1;
 
-        html1 = document.getElementById("img1").src=image1;
-        if(secondImages[0] === image1){
+        page1 = document.getElementById("img1").src=image1;
+        if(sImages[0] === image1){
             document.getElementById("alanId").innerHTML="Kazandın!!! Tebrik ederiz :) Yeni bir oyun oynamak istersen <span class='link' onclick='document.location.reload(true);'>buraya</span> tıklayabilirsin.";
         }
         guess-=1;
-    }else{
+    }
+    else{
         document.getElementById("alanId").innerHTML="Kaybettin :( Yeni bir oyun oynamak istersen <span class='link' onclick='document.location.reload(true);'>buraya</span> tıklayabilirsin.";
     }
 }
 const newGame2 = () => {
     if(guess!==0){
-        let index2 = Math.floor(Math.random()*secondImages.length);
+        let index2 = Math.floor(Math.random()*sImages.length);
         if(latestIndex === index2){
-            index2 = (index2+1)%secondImages.length;
+            index2 = (index2+1)%sImages.length;
         }
-        const image2 = secondImages[index2];
+        const image2 = sImages[index2];
         latestIndex = index2;
-        html2 = document.getElementById("img2").src=image2;
-        if(secondImages[0] === image2){
+        page2 = document.getElementById("img2").src=image2;
+        if(sImages[0] === image2){
             document.getElementById("alanId").innerHTML="Kazandın!!! Tebrik ederiz :) Yeni bir oyun oynamak istersen <span class='link' onclick='document.location.reload(true);'>buraya</span> tıklayabilirsin.";
         }
         guess-=1;
-    }else{
+    }
+    else{
         document.getElementById("alanId").innerHTML="Kaybettin :( Yeni bir oyun oynamak istersen <span class='link' onclick='document.location.reload(true);'>buraya</span> tıklayabilirsin.";
     }
 }
