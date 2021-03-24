e451c87b8a76575cea7a0c3127469b07ad71d5ef
VuslatSena <vuslatsena23@gmail.com>
2021-03-25 00:03:54 +0300

    Odev03_Son

--- odev03/public/bundle.js
+++ odev03/public/bundle.js
@@ -1,3 +1,5 @@
+//https://ceaksan.com/tr/webpack-nedir
+//Bu sayede webpack’in dosyaları nasıl / nereye ve ne şekilde bundle edeceğini belirtebiliriz. Elbette bu tanımlar için bazı dosyalar ve klasörler oluşturmamız gerekecek. src adında, içeriğinde entry point’in (örneğin index.js), bundle dosyasının (örneğin bundle.js) js, css ve html dosyalarının yer alacağı klasörleri ve dosyaları oluşturabiliriz.
 var CatGame;(()=>{
     document.getElementById("alanId"),
     document.getElementById("kazandiId"),
