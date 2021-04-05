61a3028e0fea06dd1e2979a41c19a6bfb3880b9c
VuslatSena <vuslatsena23@gmail.com>
2021-04-05 23:48:26 +0300

    Odev05 added

---
+++ odev05/src/client/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import  ReactDOM from "react-dom";
+import {Game} from "./Game";
+
+class App extends React.Component{
+    render(){
+        return (
+            <div>
+                <Game/>
+            </div>
+        );
+    }
+}
+ReactDOM.render(<App />, document.getElementById("root"));
+
---
+++ odev05/tests/Game-test.jsx
@@ -0,0 +1,73 @@
+const React = require('react');
+const {mount} = require('enzyme');
+const {Game} = require('../src/client/Game');
+
+//Kaybetme Durumu Testi
+
+test('lose',() =>{
+    const driver = mount(<Game/>);
+    //mount içinde yazan componentin adı. HTML dosyamızın içerisinde gömülmüş oluyor.
+    let card = driver.find('.card').at(0);
+    //css elemanlarını alıyoruz.
+    card.simulate('click');
+    card.find("img").prop("src");
+
+    card = driver.find('.card').at(0);
+    let srcName = card.find("img").prop("src")
+    expect(srcName === 'img/Kopek.jpg');
+
+} )
+
+//Kazanma Durumu Testi
+
+test('win',() =>{
+    const driver = mount(<Game/>);
+
+    let card = driver.find('.card').at(0);
+
+    card.simulate('click');
+    card.find("img").prop("src");
+
+    card = driver.find('.card').at(0);
+    let srcName = card.find("img").prop("src")
+    expect(srcName === 'img/Kedi.jpg');
+
+} )
+
+//Kart Tıklama Durumu Testi
+
+test('CardClick',()=>{
+    const driver = mount(<Game/>);
+
+    let card = driver.find('.card').at(0);
+
+    card.simulate('click');
+    card.find("img").prop("src");
+
+    card = driver.find('.card').at(1);
+    card.simulate('click');
+
+    card = driver.find('.card').at(2);
+    card.simulate('click');
+
+    card = driver.find('.card').at(0);
+    let srcName = card.find("img").prop("src")
+    expect(srcName === 'img/Kopek.jpg' || srcName ==='img/Kedi.jpg').toBeTruthy();
+
+} )
+
+//Yeni Oyun Durumu Testi
+
+test('newGame' ,() =>{
+    const driver = mount(<Game/>);
+    let card = driver.find('.card').at(0);
+    card.simulate('click');
+    card = driver.find('.card').at(1);
+    card.simulate('click');
+
+    let yenidenBaslat = driver.find('.link');
+    yenidenBaslat.simulate('click');
+
+
+} )
+
